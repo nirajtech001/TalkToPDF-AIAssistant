3b4711ba6735d7ef7c0f0be398147ff684c5523a
Niraj <nirajtech1001@gmail.com>
2024-10-05 00:33:23 +0530

    initial3 commit

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ st.markdown(
         width: 100%;
         background-color: #f1f1f1;
         text-align: center;
-        padding: 10px;
+        padding: 5px;
         font-size: 12px;
         color: #555;
     }
